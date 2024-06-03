7bce753b622f4ed5a86d96365ac7eaa72428e284
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-03 15:20:02 +0900

    # 57. DB로 조회수 제어

--- src/main/java/com/study/springstudy/springmvc/chap04/service/BoardService.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/service/BoardService.java
@@ -7,6 +7,8 @@ import com.study.springstudy.springmvc.chap04.dto.BoardListResponseDto;
 import com.study.springstudy.springmvc.chap04.dto.BoardWriteRequestDto;
 import com.study.springstudy.springmvc.chap04.entity.Board;
 import com.study.springstudy.springmvc.chap04.mapper.BoardMapper;
+import com.study.springstudy.springmvc.chap05.entity.ViewLog;
+import com.study.springstudy.springmvc.chap05.mapper.ViewLogMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.WebUtils;
@@ -26,6 +28,7 @@ import static com.study.springstudy.springmvc.util.LoginUtil.*;
 public class BoardService {
 
     private final BoardMapper boardMapper;
+    private final ViewLogMapper viewLogMapper;
 
     // 목록 조회 요청 중간처리
     public List<BoardListResponseDto> findList(Search page) {
@@ -56,7 +59,9 @@ public class BoardService {
     }
 
     // 상세 조회 요청 중간처리
-    public BoardDetailResponseDto detail(int bno, HttpServletRequest request, HttpServletResponse response) {
+    public BoardDetailResponseDto detail(int bno,
+                                         HttpServletRequest request,
+                                         HttpServletResponse response) {
 
         // 게시물 정보 조회
         Board b = boardMapper.findOne(bno);
@@ -64,56 +69,101 @@ public class BoardService {
         HttpSession session = request.getSession();
 
         // 비회원이거나 본인 글이면 조회수 증가 방지
-        if (!isLoggedIn(session) || isMine(b.getAccount(), getLoggedInUserAccount(session))) {
+
+        // 로그인 계정명
+        String currentUserAccount = getLoggedInUserAccount(session);
+
+        if (!isLoggedIn(session) || isMine(b.getAccount(), currentUserAccount)) {
             return new BoardDetailResponseDto(b);
         }
 
-        // 조회수가 올라가는 조건처리
-        if (shouldIncreaseViewCount(bno, request, response)) boardMapper.upViewCount(bno);
+        // 조회수가 올라가는 조건처리 (쿠키버전)
+//        if (shouldIncreaseViewCount(bno, request, response)) boardMapper.upViewCount(bno);
+//        return new BoardDetailResponseDto(b);
+
+
+        // 조회수가 올라가는 조건처리 (데이터베이스 버전)
+
+        // 1. 지금 조회하는 글이 기록에 있는지 확인
+        int boardNo = b.getBoardNo(); // 게시물 번호
+        ViewLog viewLog = viewLogMapper.findOne(currentUserAccount, boardNo);
+
+        boolean shouldIncrease = false; // 조회수 올려도 되는지??
+        ViewLog viewLogEntity = ViewLog.builder()
+                .account(currentUserAccount)
+                .boardNo(boardNo)
+                .viewTime(LocalDateTime.now())
+                .build();
+
+        if (viewLog == null) {
+            // 2. 이 게시물이 이 회원에 의해 처음 조회됨
+            viewLogMapper.insertViewLog(viewLogEntity);
+            shouldIncrease = true;
+        } else {
+            // 3. 조회기록이 있는 경우 - 1시간 이내 인지
+            // 혹시 1시간이 지난 게시물인지 확인
+            LocalDateTime oneHourAgo = LocalDateTime.now().minusHours(1);
+            if (viewLog.getViewTime().isBefore(oneHourAgo)) {
+                // 4. db에서 view_time 수정
+                viewLogMapper.updateViewLog(viewLogEntity);
+                shouldIncrease = true;
+            }
+        }
+
+        if (shouldIncrease) {
+            boardMapper.upViewCount(boardNo);
+        }
         return new BoardDetailResponseDto(b);
+
     }
 
     // 조회수 증가 여부를 판단
     /*
-        - 만약 내가 처음 조회한 상대방의 게시물이면 해당 게시물 번호로 쿠키를 생성, 쿠키안에는 생성시간을 저장, 수명은 1시간으로 설정
-        - 이후 게시물 조회시 반복해서 쿠키를 조회한 후 해당 쿠키가 존재할 시 false를 리턴하여 조회수증가를 방지
+        - 만약 내가 처음 조회한 상대방의 게시물이면
+          해당 게시물 번호로 쿠키를 생성 쿠키 안에는 생성 시간을 저장
+          수명은 1시간으로 설정
+        - 이후 게시물 조회시 반복해서 쿠키를 조회한 후 해당 쿠키가 존재할 시
+          false를 리턴하여 조회수증가를 방지
         - 쿠키 생성 예시
-        Cookie(name = view_101, 2024-06-03 14:11:30) // 이름 = 글번호, 내용 = 조회한 시간
+          Cookie(name= view_101, 2024-06-03 14:11:30)
      */
+    private boolean shouldIncreaseViewCount(int bno,
+                                            HttpServletRequest request,
+                                            HttpServletResponse response) {
 
-    private boolean shouldIncreaseViewCount(int bno, HttpServletRequest request, HttpServletResponse response) {
         // 쿠키 검사
         String cookieName = "view_" + bno;
-        Cookie viewCookie = WebUtils.getCookie(request, cookieName); // 쿠키가 있으면 리턴 쿠키, 없으면 리턴 null
+        Cookie viewCookie = WebUtils.getCookie(request, cookieName);
 
-        // 이 게시물에 대한 쿠키가 존재한다면? -> 아까 조회한 게시물이다.
+        // 이 게시물에 대한 쿠키가 존재 -> 아까 조회한 게시물
         if (viewCookie != null) {
             return false;
         }
-        // 쿠키를 생성하고
-        makeViewCookie(cookieName, response);
 
-        return true; // 조회 수 증가
+        // 쿠키 생성
+        makeViewCookie(cookieName, response);
+        return true;
     }
 
     // 조회수 쿠키를 생성하고 클라이언트에 전송하는 메서드
-    private void makeViewCookie(String cookName, HttpServletResponse response) {
-        Cookie newCookie = new Cookie(cookName, LocalDateTime.now().toString());
-        newCookie.setPath("/"); // 쿠키 사용 범위 (사이트 전체)
-        newCookie.setMaxAge(60 * 60); // 쿠키유효시간 1시간
+    private void makeViewCookie(String cookieName, HttpServletResponse response) {
+        Cookie newCookie = new Cookie(cookieName, LocalDateTime.now().toString());
+        newCookie.setPath("/"); // 쿠키 사용 범위 결정
+        newCookie.setMaxAge(60 * 60);
 
         response.addCookie(newCookie);
 
-        // 조회수를 쿠키로 제어하는게 좋지 않은 이유
-        // 쿠키에 대한 지식이 있다면 우회해서 조회수를 올릴 수 있다.
-        // 브라우저자체에서 쿠키를 차단했을 때 먹히지 않을 수 있다.
-        // 쿠키는 컴퓨터 내부에 저장되기 때문에 같은컴퓨터라면 다른계정이라도 조회수 증가하지 않는다.
-        // 쿠키는 개수제한이 있어서 (320개), 글을 그 이상 봤을 경우 먹히지 않는다.
     }
 
 
     public int getCount(Search search) {
         return boardMapper.count(search);
     }
-
 }
+
+
+// 조회수를 쿠키로 제어하는게 좋지 않은 이유
+        // 쿠키에 대한 지식이 있다면 우회해서 조회수를 올릴 수 있다.
+        // 브라우저자체에서 쿠키를 차단했을 때 먹히지 않을 수 있다.
+        // 쿠키는 컴퓨터 내부에 저장되기 때문에 같은컴퓨터라면 다른계정이라도 조회수 증가하지 않는다.
+        // 쿠키는 개수제한이 있어서 (320개), 글을 그 이상 봤을 경우 먹히지 않는다.
---
+++ src/main/java/com/study/springstudy/springmvc/chap05/entity/ViewLog.java
@@ -0,0 +1,40 @@
+package com.study.springstudy.springmvc.chap05.entity;
+
+/*
+-- 조회수 기록 관리 테이블
+CREATE TABLE view_log (
+id INT PRIMARY KEY auto_increment,
+account VARCHAR(50), -- 누가
+board_no INT, -- 어떤 게시물을
+view_time DATETIME -- 언제봤는지
+);
+
+ALTER TABLE view_log
+ADD CONSTRAINT fk_member_viewlog
+FOREIGN KEY (account)
+REFERENCES tbl_member (account);
+
+ALTER TABLE view_log
+ADD CONSTRAINT fk_board_viewlog
+FOREIGN KEY (board_no)
+REFERENCES tbl_board (board_no);
+
+SELECT * FROM view_log;
+ */
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ViewLog {
+    private long id;
+    private String account;
+    private long boardNo;
+    private LocalDateTime viewTime;
+
+}
