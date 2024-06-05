dbce878850ce01a1b4acb584376ff57a6f6aaeec
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-05 17:23:43 +0900

    # 67. 무한스크롤 로딩버그 해결

--- src/main/resources/static/assets/js/reply.js
+++ src/main/resources/static/assets/js/reply.js
@@ -1,5 +1,5 @@
 
-import { fetchInfScrollReplies, setupInfiniteScroll } from "./getReply.js";
+import { initInfScroll } from "./getReply.js";
 import { fetchReplyPost } from "./postReply.js";
 import { removeReplyClickEvent } from "./deleteReply.js";
 import { modifyReplyClickEvent } from "./modifyReply.js";
@@ -10,22 +10,18 @@ export const BASE_URL = 'http://localhost:8383/api/v1/replies';
 // ====== 실행 코드 ========
 
 // 댓글 목록 서버에서 불러오기
-// fetchReplies();
-fetchInfScrollReplies(); // 일단 1페이지 데이터 그려놓기
-// setupInfiniteScroll(); // 무한 스크롤 이벤트 등록
+initInfScroll();
 
 // 댓글 작성 이벤트 등록
-const $replyAddBtn = document.getElementById('replyAddBtn');
-$replyAddBtn?.addEventListener('click', e => {
+document.getElementById('replyAddBtn').addEventListener('click', e => {
   // 댓글 등록 로직
   fetchReplyPost();
 });
 
 // 댓글 삭제 이벤트 등록
 removeReplyClickEvent();
-
-// 댓글 수정 이벤트 등록
 modifyReplyClickEvent();
 
 // 댓글 페이지 클릭이벤트 등록
 // replyPageClickEvent();
+
